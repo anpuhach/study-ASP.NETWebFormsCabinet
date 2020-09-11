e44fc3d697f79cf0e46e3e94d46c5ab14b6210b6
Anton Puhach <tony.pug.stark@gmail.com>
2020-09-11 20:19:06 +0300

    Add project files.

---
+++ Default.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication1.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Auth</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1>ASP.NET Web Forms</h1>
+        <hr />
+        <hr />
+        <asp:Button runat="server" OnClick="ChangeTheme" Text="Apply theme:" />
+        <asp:RadioButton ID="RBThemeBlue" runat="server" GroupName="theme" Text="Blue" Checked="true" />
+        <asp:RadioButton ID="RBThemeRed" runat="server" GroupName="theme" Text="Red" />
+        <hr />
+        <p>Login:</p>
+        <asp:TextBox ID="TBLogin" runat="server" />
+        <br />
+        <p>Password:</p>
+        <asp:TextBox ID="TBPassword" runat="server" />
+        <p>Sex:</p>
+        <asp:RadioButton ID="RBSexMale" runat="server" GroupName="sex" Text="Male" />
+        <asp:RadioButton ID="RBSexFeale" runat="server" GroupName="sex" Text="Female" />
+        <asp:RadioButton ID="RBSexOther" runat="server" GroupName="sex" Text="Other" />
+        <br />
+        <p>About:</p>
+        <asp:TextBox ID="TBAbout" TextMode="multiline" Columns="40" Rows="5" runat="server"></asp:TextBox>
+        <br />
+        <p>Weekend:</p>
+        <asp:CheckBoxList ID="CBLWeekend" runat="server"></asp:CheckBoxList>
+        <hr />
+        <hr />
+        <asp:ListBox ID="LBProducts" runat="server" Rows="10" Width="400" Height="150" SelectionMode="Multiple"></asp:ListBox>
+        <asp:ListBox ID="LBCart" runat="server" Rows="10" Width="400" Height="150" SelectionMode="Multiple"></asp:ListBox>
+        <asp:Button runat="server" OnClick="AddProducts" Text="Add" />
+        <asp:Button runat="server" OnClick="AddAllProducts" Text="Add all" />
+        <asp:Button runat="server" OnClick="RemoveProducts" Text="Remove" />
+        <asp:Button runat="server" OnClick="RemoveAllProducts" Text="Remove all" />
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+            if ((string)Session["Theme"] != null)
+            {
+                Page.Theme = (string)Session["Theme"];
+                if (RBThemeBlue.Text == (string)Session["Theme"])
+                    RBThemeBlue.Checked = true;
+                else
+                    RBThemeRed.Checked = true;
+            }
+            else
+            {
+                if (RBThemeRed.Checked)
+                    Session["Theme"] = RBThemeBlue.Text;
+                else
+                    Session["Theme"] = RBThemeRed.Text;
+                Page.Theme = "Blue";
+            }
+
+            CBLWeekend.Items.Add("Mon");
+            CBLWeekend.Items.Add("Thu");
+            CBLWeekend.Items.Add("Wed");
+            CBLWeekend.Items.Add("Thu");
+            CBLWeekend.Items.Add("Fri");
+            CBLWeekend.Items.Add("Sat");
+            CBLWeekend.Items.Add("Sun");
+            foreach (ListItem li in CBLWeekend.Items)
+                if (li.Text == "Sat" || li.Text == "Sun")
+                    li.Selected = true;
+
+            LBProducts.Items.Add("Potato");
+            LBProducts.Items.Add("Tomato");
+            LBProducts.Items.Add("Cabbage");
+            LBProducts.Items.Add("Corn");
+            LBProducts.Items.Add("Onion");
+            LBProducts.Items.Add("Radish");
+            LBProducts.Items.Add("Cucumber");
+        }
+
+        protected void ChangeTheme(object sender, EventArgs e)
+        {
+            if (RBThemeBlue.Checked)
+                Session["Theme"] = "Blue";
+            else
+                Session["Theme"] = "Red";
+            Server.Transfer(Request.FilePath);
+        }
+
+        protected void AddProducts(object sender, EventArgs e)
+        {
+            int[] indexes = LBProducts.GetSelectedIndices();
+            for (int index = 0; index < indexes.Length; index++)
+            {
+                string str = LBProducts.Items[indexes[index] - index].ToString();
+                ListItem item = new ListItem(str);
+                LBProducts.Items.Remove(item);
+                LBCart.Items.Add(item);
+            }
+        }
+
+        protected void RemoveProducts(object sender, EventArgs e)
+        {
+            int[] indexes = LBCart.GetSelectedIndices();
+            for (int index = 0; index < indexes.Length; index++)
+            {
+                string str = LBCart.Items[indexes[index] - index].ToString();
+                ListItem item = new ListItem(str);
+                LBCart.Items.Remove(item);
+                LBProducts.Items.Add(item);
+            }
+        }
+
+        protected void AddAllProducts(object sender, EventArgs e)
+        {
+            for (int index = 0; index < LBProducts.Items.Count; index++)
+            {
+                string str = LBProducts.Items[index].ToString();
+                ListItem item = new ListItem(str);
+                LBCart.Items.Add(item);
+            }
+            LBProducts.Items.Clear();
+        }
+
+        protected void RemoveAllProducts(object sender, EventArgs e)
+        {
+            for (int index = 0; index < LBCart.Items.Count; index++)
+            {
+                string str = LBCart.Items[index].ToString();
+                ListItem item = new ListItem(str);
+                LBProducts.Items.Add(item);
+            }
+            LBCart.Items.Clear();
+        }
+    }
+}
